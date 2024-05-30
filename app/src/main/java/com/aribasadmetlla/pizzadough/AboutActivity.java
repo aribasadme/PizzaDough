eeef39960266245f83cab3dc5acdc9e95c1ed70e
Albert Ribas <albert_r19@hotmail.com>
2024-05-30 10:07:28 +0200

    Compose version text view from gradle variables

--- app/src/main/java/com/aribasadmetlla/pizzadough/AboutActivity.java
+++ app/src/main/java/com/aribasadmetlla/pizzadough/AboutActivity.java
@@ -7,13 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class AboutActivity extends AppCompatActivity {
-    private TextView mAbout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        String appVersionName = BuildConfig.VERSION_NAME;
+        int versionMajor = BuildConfig.VERSION_MAJOR;
+        int versionMinor = BuildConfig.VERSION_MINOR;
+        int versionPatch = BuildConfig.VERSION_PATCH;
+        int versionBuild = BuildConfig.VERSION_BUILD;
+        String appVersionName = String.format("%d.%d.%d.%d", versionMajor, versionMinor, versionPatch, versionBuild);
         String versionText = getString(R.string.version_s).concat(": ");
 
         TextView versionTextView = findViewById(R.id.aboutText);
